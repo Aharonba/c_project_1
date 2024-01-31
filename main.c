6843c18e213cb6d839bf5911712ae3c46c280df8
Aharonba <146740959+Aharonba@users.noreply.github.com>
2024-01-31 18:56:16 +0000

    commit

--- main.c
+++ main.c
@@ -13,14 +13,14 @@ int b;
       scanf("%d", &b);
 
 
-printf("The Armstrong numbers are: ");
+printf("The Armstrong numbers are:");
 
 for (int i=a; i<=b; i++)
 {
     if (isArmstrong(i))
     {
-      printf("%d", i);
-      printf(" ");
+      printf(" %d", i);
+      
     }
     
 }
@@ -28,14 +28,14 @@ for (int i=a; i<=b; i++)
 printf("\n");
 
 
-printf("The Palindromes are: ");
+printf("The Palindromes are:");
 
 for (int i=a; i<=b; i++)
 {
     if (isPalindrome(i))
     {
-      printf("%d",i);
-      printf(" ");
+      printf(" %d",i);
+    
     }
     
 }
@@ -43,15 +43,15 @@ for (int i=a; i<=b; i++)
 printf("\n");
 
 
-printf("The Prime numbers are: ");
+printf("The Prime numbers are:");
 
 
 for (int i=a; i<=b; i++)
 {
     if (isPrime(i))
     {
-      printf("%d", i);
-      printf(" ");
+      printf(" %d", i);
+    
     }
     
 }
@@ -59,18 +59,19 @@ for (int i=a; i<=b; i++)
 printf("\n");
 
 
-printf("The Strong numbers are: ");
+printf("The Strong numbers are:");
 
 for (int i=a; i<=b; i++)
 {
     if (isStrong(i))
     {
-      printf("%d", i);
-      printf(" ");
+      printf(" %d", i);
+   
     }
     
 }
 
+printf("\n");
 
 }
 
